2fa4065645e944fc145d1cc569dcd476bbd06902
wenqing <wenqing@mogujie.com>
2016-07-15 18:28:52 +0800

    wenqing: 完成react-router接入

--- src/entry.js
+++ src/entry.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import { Router, Route, hashHistory } from 'react-router';
+
 class App extends React.Component {
     render() {
         return <p> Hello { this.props.name } </p>;
     }
 }
 
-render(<App name="React!!" />, document.getElementById('app') );
+render(
+    (<Router history={ hashHistory }>
+        <Route path="/" component={ App } />
+    </Router>), document.getElementById('app')
+);
+
+//render(<App name="React!!" />, document.getElementById('app') );
